f0c13f18212208e04dc454b21d61dcee5d62f9b3
akinayturan <akinayturan@gmail.com>
2020-08-13 14:26:39 +0300

    init2

--- components/Contact.js
+++ components/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
                                 <div className="contact-info-one__icon"><i className="kipso-icon-contact"></i>
                                 </div>
                                 <h2 className="contact-info-one__title">Publisher</h2>
-                                <p className="contact-info-one__text">Yasin Akın Ayturan, Konya, Türkiye <br />ijepem@ijepem.com</p>
+                                <p className="contact-info-one__text">Yasin Akın Ayturan, Konya, Turkey <br />ijepem@ijepem.com</p>
                             </div>
                         </div>
                         <div className="col-lg-6">
