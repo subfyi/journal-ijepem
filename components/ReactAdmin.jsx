c31bd5338ef1ef3cff29151925400591c612dc39
akinayturan <akinayturan@gmail.com>
2023-10-25 14:57:43 +0300

    Delete unused provider files
    The files LoginProvider, UploadProvider and UserProvider were deleted as they were not used anywhere in the project. Public files htaccess and sitemap_1.xml were also removed as they were redundant. Removing these unused files helps keep the codebase clean and easy to maintain.

--- components/ReactAdmin.jsx
+++ components/ReactAdmin.jsx
@@ -1,9 +1,6 @@
 'use client'
 
 import { App, AuthProvider } from 'react-admin-base'
-import LoginProvider from '@/providers/LoginProvider'
-import { UserProvider } from '@/providers/UserProvider'
-import UploadConfig from '@/providers/UploadProvider'
 import { IntlProvider } from 'react-intl'
 
 const languages = {
@@ -20,13 +17,7 @@ export default function ReactAdminApp({ children }) {
   return <App id='iseser' endpoint={process.env.ENDPOINT} name='ISESER'>
     <IntlProvider locale='en' messages={languages.en.messages}>
       <AuthProvider tokenEndpoint='/oauth/token' client_id='2' client_secret='JjPIsb7TNCf7ysEfs0JDhl5XXBgIVh6dMRLMCrb9'>
-        <UploadConfig>
-          <UserProvider>
-            <LoginProvider>
               {children}
-            </LoginProvider>
-          </UserProvider>
-        </UploadConfig>
       </AuthProvider>
     </IntlProvider>
   </App>
