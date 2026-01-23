3edf711b72a3478aa2ac17de7980f950959fb18d
akinayturan <akinayturan@gmail.com>
2026-01-23 12:47:55 +0300

    Update breadcrumb link text from 'Anasayfa' to 'Home'

--- layout/Breadcrumb.tsx
+++ layout/Breadcrumb.tsx
@@ -6,7 +6,7 @@ export function PageHeader(props: { title: string }) {
       <section className="inner-banner">
         <div className="container">
           <ul className="list-unstyled thm-breadcrumb">
-            <li><Link href="/">Anasayfa</Link></li>
+            <li><Link href="/">Home</Link></li>
             <li className="active">{ props.title }</li>
           </ul>
           <h2 className="inner-banner__title">{ props.title }</h2>
